6eb619fa75c2eb88a144d1efb47af33469133166
Vitinzkx7 <victornex2006@hotmail.com>
2025-05-26 08:56:06 -0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// server/src/index.js - conteúdo exemplo
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
